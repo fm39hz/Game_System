feddeede5a84dfa67957e509790406f4539c4687
fm39hz <hitpoint2k3@gmail.com>
2023-08-08 22:35:11 +0700

    added timer to Effect

--- Component/DamageSystem/Effect.cs
+++ Component/DamageSystem/Effect.cs
@@ -11,9 +11,20 @@ public partial class Effect : Node
 	[Signal]
 	public delegate void EffectDiscardedEventHandler();
 
+	[Export] public double CountDown { get; set; }
+	private Timer Timer { get; set; }
+	
+
+	public override void _EnterTree()
+	{
+		Timer.ProcessCallback = Timer.TimerProcessCallback.Idle;
+		Timer.OneShot = true;
+		Timer.Timeout += Discard;
+	}
 	public virtual void Apply()
 	{
 		EmitSignal(SignalName.EffectApplied);
+		Timer.Start(CountDown);
 	}
 
 	public virtual void Discard()

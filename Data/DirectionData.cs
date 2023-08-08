95367b68d83ad02aa0b0738f8baf9f3b9961abe4
fm39hz <hitpoint2k3@gmail.com>
2023-08-08 22:34:57 +0700

    fixed system bad design

--- Data/DirectionData.cs
+++ Data/DirectionData.cs
@@ -1,56 +1,25 @@
 using Godot;
 using GameSystem.Utility.Direction;
-using System.Collections.Generic;
-using System;
 
 namespace GameSystem.Data.Instance;
 
-public class DirectionData
+public partial class DirectionData : Node
 {
-	public Dictionary<int, Vector2> DirectionContainer { get; private set; }
-	public Dictionary<int, float> RadiantContainer { get; private set; }
 	public int AsNumber { get; private set; }
 	public Vector2 AsVector { get; private set; }
 	public float AsRadiant { get; private set; }
 
-	public DirectionData()
-	{
-		DirectionContainer = new Dictionary<int, Vector2>(8)
-		{
-			{ 0, Vector2.Down },
-			{ 1, Vector2.Right },
-			{ 2, Vector2.Up },
-			{ 3, Vector2.Left },
-			{ 4, Vector2.Down + Vector2.Right },
-			{ 5, Vector2.Right + Vector2.Up },
-			{ 6, Vector2.Up + Vector2.Left },
-			{ 7, Vector2.Left + Vector2.Down }
-		};
-		var _pi = MathF.PI;
-		RadiantContainer = new Dictionary<int, float>(8)
-		{
-			{ 0, _pi / 2 },
-			{ 1, 0 },
-			{ 2, -_pi / 2 },
-			{ 3, 2 * _pi },
-			{ 4, _pi / 4 },
-			{ 5, -_pi / 4 },
-			{ 6, -3 * _pi / 4 },
-			{ 7, 3 * _pi / 4 }
-		};
-	}
-
 	public void SetDirection(int input)
 	{
 		AsNumber = input;
-		AsVector = DirectionConverter.ToDirection(input);
-		AsRadiant = DirectionConverter.ToRadiant(input);
+		AsVector = Direction.ToDirection(input);
+		AsRadiant = Direction.ToRadian(input);
 	}
 
 	public void SetDirection(Vector2 input)
 	{
 		AsVector = input;
-		AsNumber = DirectionConverter.ToDirection(input);
-		AsRadiant = DirectionConverter.ToRadiant(AsNumber);
+		AsNumber = Direction.ToDirection(input);
+		AsRadiant = Direction.ToRadian(AsNumber);
 	}
 }
---
+++ Utility/Direction.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+
+namespace GameSystem.Utility.Direction;
+
+public static class Direction
+{
+	private static readonly Dictionary<int, Vector2> DirectionContainer = new(8)
+	{
+		{ 0, Vector2.Down },
+		{ 1, Vector2.Right },
+		{ 2, Vector2.Up },
+		{ 3, Vector2.Left },
+		{ 4, Vector2.Down + Vector2.Right },
+		{ 5, Vector2.Right + Vector2.Up },
+		{ 6, Vector2.Up + Vector2.Left },
+		{ 7, Vector2.Left + Vector2.Down }
+	};
+	private static readonly Dictionary<int, float> RadianContainer = new(8)
+	{
+		{ 0, MathF.PI / 2 },
+		{ 1, 0 },
+		{ 2, -MathF.PI / 2 },
+		{ 3, 2 * MathF.PI },
+		{ 4, MathF.PI / 4 },
+		{ 5, -MathF.PI / 4 },
+		{ 6, -3 * MathF.PI / 4 },
+		{ 7, 3 * MathF.PI / 4 }
+	};
+	public static int ToDirection(Vector2 input)
+	{
+		var _target = 0;
+			foreach (var _direction in DirectionContainer)
+			{
+				if (MathF.Round(input.AngleTo(_direction.Value)) == 0)
+				{
+					_target = _direction.Key;
+					break;
+				}
+			}
+		return _target;
+	}
+
+	public static Vector2 ToDirection(int input)
+	{
+		var _target = Vector2.Zero;
+			if (input < 0 || input > 7)
+			{
+				return _target;
+			}
+			foreach (var _direction in DirectionContainer)
+			{
+				if (input == _direction.Key)
+				{
+					_target = _direction.Value.Normalized();
+					break;
+				}
+			}
+		return _target;
+	}
+
+	public static float ToRadian(int input)
+	{
+		var _target = 0.0f;
+			foreach (var _radian in RadianContainer)
+			{
+				if (input == _radian.Key)
+				{
+					_target = _radian.Value;
+				}
+			}
+		return _target;
+	}
+}

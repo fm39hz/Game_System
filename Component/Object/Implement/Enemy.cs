c65e4950afd34b7d93266a14aa91d9b589b5c4fe
fm39hz <hitpoint2k3@gmail.com>
2023-08-18 21:27:20 +0700

    Created temp Enemy

---
+++ Component/Object/Implement/Enemy.cs
@@ -0,0 +1,9 @@
+using Godot;
+using GameSystem.Component.Object.Composition;
+
+namespace GameSystem.Component.Object.Implement;
+
+public partial class Enemy : Creature
+{
+	public Creature ChasingTarget { get; set; }
+}

cb87051b2dfdce302442897a422ab6be7dc7c69f
fm39hz <hitpoint2k3@gmail.com>
2023-08-30 22:25:00 +0700

    Improve loading time

--- Component/Object/Compositor/CreatureCompositor.cs
+++ Component/Object/Compositor/CreatureCompositor.cs
@@ -17,7 +17,7 @@ public partial class CreatureCompositor : ObjectCompositor {
 		if (Target is not Creature _target) {
 			throw new InvalidCastException("Target must be Creature");
 		}
-		Hurtbox = _target.GetFirstChildOfType<HurtBox>();
+		Hurtbox = _target.GetFirstChild<HurtBox>();
 		SpriteSheet.PolygonChanged += Hurtbox.UpdateCollision;
 	}
 
@@ -47,17 +47,15 @@ public partial class CreatureCompositor : ObjectCompositor {
 
 			var _position = new Vector2I(_horizontalIndex, _verticalIndex);
 			var _polys = _bitmap.OpaqueToPolygons(new Rect2I(_position, _width, _height), 0.42f);
-			foreach (var _poly in _polys) {
+				if (_polys.Count == 0) continue;
+            var _poly = _polys[0];
 				for (var _i = 0; _i < _poly.Length; _i++) {
 					_poly[_i] += SpriteSheet.Position;
 				}
-
-				var _shape = new CollisionPolygon2D {
-					Polygon = _poly
-				};
-
+			var _shape = new CollisionPolygon2D {
+				Polygon = _poly
+			};
 				_information.ShapePool.TryAdd(_frame, _shape);
-			}
 		}
 	}
 

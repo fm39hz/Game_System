20e943086d90b2ea4a5fcb42fad45ba164312394
fm39hz <hitpoint2k3@gmail.com>
2023-08-30 23:48:24 +0700

    Dispose ShapePool when Hurtbox exit tree

--- Component/DamageSystem/HurtBox.cs
+++ Component/DamageSystem/HurtBox.cs
@@ -1,4 +1,5 @@
 using GameSystem.Component.Object.Compositor;
+using GameSystem.Data.Global;
 using GameSystem.Data.Instance;
 using GameSystem.Utils;
 using Godot;
@@ -15,17 +16,26 @@ public partial class HurtBox : Area2D
 		Compositor = GetOwner<CreatureCompositor>();
 		CollisionLayer = 2;
 		CollisionMask = 2;
-		Modulate = Colors.Red;
+		Modulate = Colors.Green;
+	}
+
+	public override void _ExitTree()
+	{
+		foreach (var _item in ((CreatureData)Compositor.Information).ShapePool)
+		{
+			_item.Value.Dispose();
+		}
 	}
 
 	public void UpdateCollision(int frame)
 	{
+		GetTree().DebugCollisionsHint = GlobalStatus.Instance.IsDebug;
 		this.RemoveAllChild();
 		foreach (var (_frame, _collisionPolygon2D) in ((CreatureData)Compositor.Information).ShapePool)
 		{
 			if (_frame == frame)
 			{
-				AddChild(_collisionPolygon2D, true);
+				AddChild(_collisionPolygon2D);
 			}
 		}
 	}

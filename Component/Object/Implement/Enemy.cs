83a0df379820927e3234138471da9df86b66f68a
fm39hz <hitpoint2k3@gmail.com>
2023-09-07 00:09:15 +0700

    added targeting zone

--- Component/Object/Implement/Enemy.cs
+++ Component/Object/Implement/Enemy.cs
@@ -1,15 +1,16 @@
 using Godot;
 using GameSystem.Component.Object.Composition;
 using GameSystem.Utils;
+using Godot.Collections;
 namespace GameSystem.Component.Object.Implement;
 
 public partial class Enemy : Creature
 {
-	[Export] public Creature ChasingTarget { get; set; }
+  [Export] public Array<Creature> Target { get; private set; }
   public Area2D TargetingZone { get; set; }
   public override void _Ready()
-    {
-      base._Ready();
-      TargetingZone = this.GetFirstChild<Area2D>();
-    }
+  {
+    base._Ready();
+    TargetingZone = this.GetFirstChild<Area2D>();
+  }
 }

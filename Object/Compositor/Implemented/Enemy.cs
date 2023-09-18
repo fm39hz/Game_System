5507cd1b4ae0b83946e680ff65c1430bc170b87f
fm39hz <hitpoint2k3@gmail.com>
2023-09-18 16:41:34 +0700

    made player default target

--- Object/Compositor/Implemented/Enemy.cs
+++ Object/Compositor/Implemented/Enemy.cs
@@ -1,15 +1,14 @@
 using Godot;
-using GameSystem.Component.Object.Composition;
 using GameSystem.Component.Object.Compositor;
 using GameSystem.Utils;
-using Godot.Collections;
+using GameSystem.Object.Compositor.Implemented;
 
 namespace GameSystem.Component.Object.Implemented;
 
 [GlobalClass]
 public partial class Enemy : CreatureCompositor
 {
-	[Export] public Array<Creature> Target { get; private set; }
+	public Player Target { get; private set; }
 	public Area2D TargetingZone { get; set; }
 
 	public override void _Ready()

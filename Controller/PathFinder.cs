8e3c3b7b1f89ebedfc55784425028f742e575c37
fm39hz <hitpoint2k3@gmail.com>
2023-09-23 17:56:16 +0700

    create basic prompt for pathfinder

--- Controller/PathFinder.cs
+++ Controller/PathFinder.cs
@@ -1,9 +1,12 @@
 using Godot;
 using GameSystem.Object.Compositor;
+using System.Collections.Generic;
 
 namespace GameSystem.Controller;
 
 public class PathFinder
 {
-    public CreatureCompositor Compositor { get; set; }
+    public CreatureCompositor Object { get; set; }
+    public Vector2 TargetLocation { get; set; }
+    public Dictionary<Vector2, Vector2> Objectiles { get; set; }
 }

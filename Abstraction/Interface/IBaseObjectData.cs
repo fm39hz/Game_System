9b709ed24c8246ac354e03d24127a4a5a703c5a3
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-03-09 17:17:06 +0700

    feat: add IBaseObjectData for fragment using

---
+++ Abstraction/Interface/IBaseObjectData.cs
@@ -0,0 +1,23 @@
+﻿using GameSystem.Component.FiniteStateMachine;
+using GameSystem.Data.Instance;
+using Godot;
+
+namespace GameSystem.Abstraction;
+
+public interface IBaseObjectData
+{
+	/// <summary>
+	///     Owner current state
+	/// </summary>
+	public State CurrentState { get; set; }
+
+	/// <summary>
+	///     Owner direction facing
+	/// </summary>
+	public DirectionData Direction { get; set; }
+
+	/// <summary>
+	///     Owner Location
+	/// </summary>
+	public Vector2 Location { get; set; }
+}

9b12254f37b58aef3f15f652eb3b3962f711a5b0
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-03-09 15:58:38 +0700

    chore: rename VirtualInstance to Abstraction

--- Abstraction/Interface/IDirectionalInput.cs
+++ Abstraction/Interface/IDirectionalInput.cs
@@ -1,9 +1,9 @@
 ﻿using Godot;
 
-namespace GameSystem.VirtualInstance;
+namespace GameSystem.Abstraction;
 
 public interface IDirectionalInput
 {
 	public Vector2 GetMovementVector(Vector2 inputVector);
-	// public Vector2 GetJumpVector(Vector2 inputVector);
+	public Vector2 GetJumpVector(Vector2 inputVector);
 }

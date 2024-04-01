3513d33f37bf2e3152c5022699be9870b0c8bfe0
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-04-01 20:48:30 +0700

    refactor: removed unnecessary implement

--- GameSystem.Core/src/Object/Root/ItemRoot.cs
+++ GameSystem.Core/src/Object/Root/ItemRoot.cs
@@ -3,20 +3,4 @@ using GameSystem.Core.Object.PhysicsBody.Base;
 
 namespace GameSystem.Core.Object.Root;
 
-public partial class ItemRoot : ObjectRoot<ItemData, Item>
-{
-	public override void PlayAnimation()
-	{
-		throw new NotImplementedException();
-	}
-
-	public override void UpdateData(double delta)
-	{
-		throw new NotImplementedException();
-	}
-
-	public override void InitData()
-	{
-		throw new NotImplementedException();
-	}
-}
+public partial class ItemRoot : ObjectRoot<ItemData, Item>;

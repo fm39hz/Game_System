a242d67c131ddc211064d9700130e3f9897a0c59
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-04-07 00:47:41 +0700

    feat: implement IMachinaryState

--- Component/FiniteStateMachine/StaticState.cs
+++ Component/FiniteStateMachine/StaticState.cs
@@ -1,11 +1,22 @@
+using GameSystem.Component.FiniteStateMachine;
 using Godot;
 
 namespace GameSystem.Core.Component.FiniteStateMachine;
 
 [GlobalClass]
-public partial class StaticState : State
+public partial class StaticState : State, IMachinaryState
 {
 	[ExportCategory("Motion")] [Export] public float MaxSpeed { get; protected set; }
 	[Export] public float Acceleration { get; protected set; }
 	[Export] public float Friction { get; protected set; }
+
+	public void EnteredMachine()
+	{
+		//TODO: set up state machine
+	}
+
+	public void ExitMachine()
+	{
+		//TODO: clean up state machine
+	}
 }

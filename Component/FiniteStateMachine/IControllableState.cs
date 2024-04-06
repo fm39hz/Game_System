ac37cf691daae3aaa658834206585521403c61d4
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-04-07 00:30:09 +0700

    feat: add small interface

---
+++ Component/FiniteStateMachine/IControllableState.cs
@@ -0,0 +1,6 @@
+namespace GameSystem.Component.FiniteStateMachine;
+public interface IControllableState 
+{ 
+	public void ResetCondition();
+	public void SetCondition(bool condition);
+}
---
+++ Component/FiniteStateMachine/IMachinaryState.cs
@@ -0,0 +1,7 @@
+namespace GameSystem.Component.FiniteStateMachine;
+
+public interface IMachinaryState
+{
+	public void EnteredMachine();
+	public void ExitMachine();
+}

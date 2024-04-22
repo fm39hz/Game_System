6a3a86c8b99644f102f501c62c526a666efe4c30
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-04-22 17:59:29 +0700

    feat: move implement back to parent space

--- Object/PhysicsBody/Creature.cs
+++ Object/PhysicsBody/Creature.cs
@@ -1,11 +1,16 @@
-﻿using GameSystem.Core.Object.Root;
+﻿using GameSystem.Core.Component.DamageSystem;
+using GameSystem.Core.Data;
+using GameSystem.Core.Object.Root;
+using GameSystem.Core.Utils;
 using Godot;
+using Prototype.GameSystem.Object;
 
 namespace GameSystem.Core.Object.PhysicsBody;
 
 [GlobalClass]
-public partial class Creature : CharacterBody2D
+public partial class Creature : CharacterBody2D, IContainer
 {
+	[Export] public CreatureData Information { get; set; } = new();
 	public CreatureRoot Root { get; set; }
 
 	public override void _EnterTree()
@@ -16,5 +21,16 @@ public partial class Creature : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		MoveAndSlide();
+	}
+
+	public void InitData()
+	{
+		var _spriteSheet = Root.SpriteSheet;
+		Root.Hurtbox = this.GetFirstChild<HurtBox>();
+		var _bitmap = new Bitmap();
+		Root.SpriteSheet.PolygonChanged += Root.Hurtbox.UpdateCollision;
+		_bitmap.CreateFromImageAlpha(_spriteSheet.Texture.GetImage());
+		Information.ShapePool = PolygonCreator.GetArea(_spriteSheet, _bitmap, Name);
+		Root.Information = Information;
 	}
 }
